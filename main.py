21d29bf5b4dd2f06b04dc2b36058f1ebaf055765
TinNguyen <ductin16111@gmail.com>
2022-06-15 11:24:22 +0300

    create main.py file

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+
+# Initialize the pygame
+pygame.init()
+
+# create the screen
+screen = pygame.display.set_mode((800, 600))    # 800px height, 600px width
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
