92320a54c27f9bbbf647d73518406f7dd5169fdc
TinNguyen <ductin16111@gmail.com>
2022-06-21 17:08:55 +0300

    change title, icon and background color of display screen

--- main.py
+++ main.py
@@ -6,8 +6,19 @@ pygame.init()
 # create the screen
 screen = pygame.display.set_mode((800, 600))    # 800px height, 600px width
 
+# Title and Icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('spaceship.png')
+pygame.display.set_icon(icon)
+
+# Game Loop
 running = True
 while running:
+    pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    
+    # background color RGB
+    screen.fill((255,0,0))
+    
