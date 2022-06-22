69823e592e6088b7b22611606e391a03a1b02540
TinNguyen <ductin16111@gmail.com>
2022-06-22 17:27:33 +0300

    add images into the game

--- main.py
+++ main.py
@@ -4,21 +4,30 @@ import pygame
 pygame.init()
 
 # create the screen
-screen = pygame.display.set_mode((800, 600))    # 800px height, 600px width
+screen = pygame.display.set_mode((800, 600))    # 800px width, 600px height
 
 # Title and Icon
 pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load('spaceship.png')
 pygame.display.set_icon(icon)
 
+# PLayer
+playerImg = pygame.image.load('ss1.png')
+playerX = 370
+playerY = 480
+
+def player():
+    screen.blit(playerImg, (playerX, playerY))
+
 # Game Loop
 running = True
 while running:
     pygame.display.update()
+    # background color RGB
+    screen.fill((0,0,0))    # screen should be drawn first, below all others
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     
-    # background color RGB
-    screen.fill((255,0,0))
+    player()
     
